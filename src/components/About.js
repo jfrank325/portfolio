3afdd548849198bdedde34ccdf1849d1ab62357a
Jason Franklin <jfrank325@gmail.com>
2020-05-01 16:57:09 +0200

    real content finished

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,19 @@ import ScrollAnimation from "react-animate-on-scroll"
 const About = () => {
   return (
     <ScrollAnimation animateIn="opaque">
-      <div className="about-container opaque" id="about">
-        <div className="picture">
+      <div className="about-container opaque">
+        <div className="picture" id="about">
           <img src={Image} alt="Jason's profile" />
         </div>
         <div className="content">
           <h1>About Me</h1>
-          <p>This is letting you know what I'm about</p>
+          <p>
+            I'm a full-stack web developer with a passion for creating unique
+            user experiences with beautiful code and the newest technologies. I
+            bring a special level of creativity and aesthetic from my past
+            career as a dancer and choreographer. I'd love to hear from you
+            about new job opportunities, collaborations, or projects.
+          </p>
         </div>
       </div>
     </ScrollAnimation>
