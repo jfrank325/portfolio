52576fdf97affbd6fca1a9d39743033c7aa30577
Jason Franklin <jfrank325@gmail.com>
2020-06-15 15:26:08 +0200

    metadata fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import "../styles/styles.scss"
 import Home from "../components/Home"
 
 import Layout from "../components/layout"
-// import SEO from "../components/seo"
+import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <SEO title="Home" />
     <Home />
   </Layout>
 )
