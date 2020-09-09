7699482637aac0a4b05af29cd3ed4cc1c94cf43e
Jason Franklin <jfrank325@gmail.com>
2020-09-09 15:13:32 +0200

    removing scroll to top

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react"
-import Arrow from "../images/rightarrow.png"
+// import React, { useState } from "react"
+// import Arrow from "../images/rightarrow.png"
 
-const ScrollToTop = () => {
-  const [showScroll, setShowScroll] = useState(false)
+// const ScrollToTop = () => {
+//   const [showScroll, setShowScroll] = useState(false)
 
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 400) {
-      setShowScroll(true)
-    } else if (showScroll && window.pageYOffset <= 400) {
-      setShowScroll(false)
-    }
-  }
-  const scrollTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" })
-  }
-  window.addEventListener("scroll", checkScrollTop)
-  return (
-    <div
-      className="scroll-container"
-      onClick={scrollTop}
-      style={{ height: 40, display: showScroll ? "flex" : "none" }}
-    >
-      <img src={Arrow} alt="Scroll To Top" />
-    </div>
-  )
-}
+//   const checkScrollTop = () => {
+//     if (!showScroll && window.pageYOffset > 400) {
+//       setShowScroll(true)
+//     } else if (showScroll && window.pageYOffset <= 400) {
+//       setShowScroll(false)
+//     }
+//   }
+//   const scrollTop = () => {
+//     window.scrollTo({ top: 0, behavior: "smooth" })
+//   }
+//   window.addEventListener("scroll", checkScrollTop)
+//   return (
+//     <div
+//       className="scroll-container"
+//       onClick={scrollTop}
+//       style={{ height: 40, display: showScroll ? "flex" : "none" }}
+//     >
+//       <img src={Arrow} alt="Scroll To Top" />
+//     </div>
+//   )
+// }
 
-export default ScrollToTop
+// export default ScrollToTop
